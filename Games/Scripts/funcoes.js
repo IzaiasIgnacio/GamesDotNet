cefaf81733bcd817020119c571b0e3d98b380843
Izaias <Izaias@BATTOUSAI>
2017-01-28 15:36:30 -0200

    Busca

--- Games/Scripts/funcoes.js
+++ Games/Scripts/funcoes.js
@@ -1,9 +1,2 @@
 ﻿$(function() {
-    $("#busca").keyup(function () {
-        $.post("/Jquery/BuscarJogoEntityJquery", { search: $(this).val() },
-        function (resposta) {
-            $("#lista_resultados_busca").html(resposta);
-            $("#lista_resultados_busca").slideDown();
-        });
-    });
 });
