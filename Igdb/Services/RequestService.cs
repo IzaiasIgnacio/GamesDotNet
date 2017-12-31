11e1211bd1b1f55a208fab0ad8495e0c3398774d
Izaias <Izaias@BATTOUSAI>
2017-12-30 23:34:53 -0200

    igdb

--- Igdb/Services/RequestService.cs
+++ Igdb/Services/RequestService.cs
@@ -4,12 +4,16 @@ using System.Net;
 
 namespace Igdb.Services {
     public class RequestService {
+
+        public string requestUrl = "https://api-2445582011268.apicast.io";
+
         public string DoRequest(string url) {
             string resposta = string.Empty;
             HttpWebRequest get = (HttpWebRequest)WebRequest.Create(url);
             get.AutomaticDecompression = DecompressionMethods.GZip;
             get.Accept = "application/json";
-            get.Headers["X-Mashape-Key"] = "UHp7vqCOoumshnITXnFRvjeVRX6Bp1Euzr2jsnSUkkS2pxGYC2";
+            get.Headers["user-key"] = "fe07e6780755d0352df34380835b942f";
+            
             using (HttpWebResponse response = (HttpWebResponse)get.GetResponse())
             using (Stream stream = response.GetResponseStream())
             using (StreamReader reader = new StreamReader(stream)) {
@@ -20,42 +24,41 @@ namespace Igdb.Services {
         }
 
         public string RequestBuscaGame(BuscaGameRequest request) {
-            string url = @"https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields="+request.Fields+"&filter="+request.Platforms+
-                "&limit="+request.Limit+"&search="+request.Search;
+            string url = requestUrl+"/games/?fields="+request.Fields+"&filter="+request.Platforms+"&limit="+request.Limit+"&search="+request.Search;
 
             return DoRequest(url);
         }
 
         public string RequestDadosGame(DadosGameRequest request) {
             string resposta = string.Empty;
-            string url = @"https://igdbcom-internet-game-database-v1.p.mashape.com/games/"+request.Id+"?fields="+request.Fields;
+            string url = requestUrl+"/games/"+request.Id+"?fields="+request.Fields;
 
             return DoRequest(url);
         }
 
         public string RequestDadosDeveloperPublisherGame(DadosDeveloperPublisherRequest request) {
             string resposta = string.Empty;
-            string url = @"https://igdbcom-internet-game-database-v1.p.mashape.com/companies/"+string.Join(",",request.Ids)+"?fields=" + request.Fields;
+            string url = requestUrl + "/companies/" + string.Join(",",request.Ids)+"?fields=" + request.Fields;
 
             return DoRequest(url);
         }
 
         public string RequestDadosPlatform(DadosPlatformRequest request) {
             string resposta = string.Empty;
-            string url = @"https://igdbcom-internet-game-database-v1.p.mashape.com/platforms/" + string.Join(",", request.Ids) + "?fields=" + request.Fields;
+            string url = requestUrl + "/platforms/" + string.Join(",", request.Ids) + "?fields=" + request.Fields;
 
             return DoRequest(url);
         }
 
         public string RequestDadosGenre(DadosGenreRequest request) {
             string resposta = string.Empty;
-            string url = @"https://igdbcom-internet-game-database-v1.p.mashape.com/genres/" + string.Join(",", request.Ids) + "?fields=" + request.Fields;
+            string url = requestUrl + "/genres/" + string.Join(",", request.Ids) + "?fields=" + request.Fields;
 
             return DoRequest(url);
         }
 
         public string RequestGameSeries(DadosGameSeriesRequest request) {
-            string url = @"https://igdbcom-internet-game-database-v1.p.mashape.com/collections/" + request.Id + "?fields=" + request.Fields;
+            string url = requestUrl + "/collections/" + request.Id + "?fields=" + request.Fields;
 
             return DoRequest(url);
         }
